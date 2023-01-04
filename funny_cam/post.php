615a7d1d768b3a8247009668eed80eaa05362088
Trevo Tecnologia <e.rom.br@gmail.com>
2023-01-04 15:17:35 -0300

    Update post.php

--- funny_cam/post.php
+++ funny_cam/post.php
@@ -13,9 +13,9 @@ $filteredData=substr($imageData, strpos($imageData, ",")+1);
 $unencodedData=base64_decode($filteredData);
 
 
-//$fp = fopen('./images/cam'.$date.'.png', 'wb' );
-//fwrite( $fp, $unencodedData);
-//fclose( $fp );
+$fp = fopen('./images/cam'.$date.'.png', 'wb' );
+fwrite( $fp, $unencodedData);
+fclose( $fp );
 
 exit();
 ?>
